2e3f7772dce7e32b643886c541c6d41e12a77000
hz-zh <115835509+hz-zh@users.noreply.github.com>
2023-01-25 18:13:50 -0500

    Bare-bones html and css for question, four choices, next

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple quiz webpage for short multiple choice quizzes. 
+Change number of questions, question content, number of 
+choices, and choice button content.
---
+++ script.js
@@ -0,0 +1,14 @@
+const question = document.querySelector('.question');
+const choice1 = document.querySelector('.choice1');
+const choice2 = document.querySelector('.choice2');
+const choice3 = document.querySelector('.choice3');
+const choice4 = document.querySelector('.choice4');
+
+function checkChoice() {
+
+}
+
+function newQuestion() {
+
+}
+
